50f5591967edddd6c4658ff1c9f734ca871fc669
DREWiex <aleon88@gmail.com>
2023-02-26 02:37:11 +0100

    Versión A del ejercicio bastante avanzada

---
+++ js/main.js
@@ -0,0 +1,153 @@
+//TODO: Práctica filtros
+
+document.addEventListener('DOMContentLoaded', () => {
+
+
+    //*** VARIABLES ***//
+
+    const fragment = document.createDocumentFragment();
+    const urlBase = 'assets/';
+
+    //* capturas *//
+    const capBotones = document.querySelector('#botones');
+    const capTextoTag = document.querySelector('#text-tag');
+    const capTituloTag = document.querySelector('#title-tag');
+    const capFotos = document.querySelector('#fotos');
+
+    //* arrays *//
+    const arrayBotones = [
+        ['Playa', 'btn1'],
+        ['Edificio', 'btn2'],
+        ['Montaña', 'btn3'],
+        ['Señales', 'btn4'],
+        ['Cositas', 'btnNull']
+    ];
+
+    const arrayFotos = [
+        {id: 'btn1', tag: 'Playa', url: 'viajes-1.jpg', title: 'Foto de viaje 1', alt: 'Foto de viaje 1', header: 'Viaje 1'},
+        {id: 'btn1', tag: 'Playa', url: 'viajes-2.jpg', title: 'Foto de viaje 2', alt: 'Foto de viaje 2', header: 'Viaje 2'},
+        {id: 'btn4', tag: 'Señales', url: 'viajes-3.jpg', title: 'Foto de viaje 3', alt: 'Foto de viaje 3', header: 'Viaje 3'},
+        {id: 'btn2', tag: 'Edificio', url: 'viajes-4.jpg', title: 'Foto de viaje 4', alt: 'Foto de viaje 4', header: 'Viaje 4'},
+        {id: 'btn2', tag: 'Edificio', url: 'viajes-5.jpg', title: 'Foto de viaje 5', alt: 'Foto de viaje 5', header: 'Viaje 5'},
+        {id: 'btn3', tag: 'Montaña', url: 'viajes-6.jpg', title: 'Foto de viaje 6', alt: 'Foto de viaje 6', header: 'Viaje 6'},
+        {id: 'btn3', tag: 'Montaña', url: 'viajes-7.jpg', title: 'Foto de viaje 7', alt: 'Foto de viaje 7', header: 'Viaje 7'}
+    ];
+
+
+
+
+    //*** EVENTOS ***//
+
+    capBotones.addEventListener('click', ({target}) => {
+
+        if(target.matches('#btn1')){ //* botón playa
+            const id = target.id; //? ¿quizás utilizar .contains() para capturar y pintar los que compartan la misma propiedad tag, por ej.
+            filtrarTag(id); //! pendiente cambiar el id por data-id (falta pintarlo en pintarBotones()) para imprimir el nombre que corresponda a la etiqueta
+            pintarFotos(id);
+        }
+
+        if(target.matches('#btn2')){ //* botón edificio
+            const id = target.id;
+            filtrarTag(id); //! pendiente cambiar el id por data-id (falta pintarlo en pintarBotones()) para imprimir el nombre que corresponda a la etiqueta
+            pintarFotos(id);
+        }
+
+        if(target.matches('#btn3')){ //* botón montaña
+            const id = target.id;
+            filtrarTag(id); //! pendiente cambiar el id por data-id (falta pintarlo en pintarBotones()) para imprimir el nombre que corresponda a la etiqueta
+            pintarFotos(id);
+        }
+
+        if(target.matches('#btn4')){ //* botón señales
+            const id = target.id;
+            filtrarTag(id); //! pendiente cambiar el id por data-id (falta pintarlo en pintarBotones()) para imprimir el nombre que corresponda a la etiqueta
+            pintarFotos(id);
+        }
+
+        if(target.matches('#btnNull')){ //* botón cositas (null)
+            const id = target.id;
+            filtrarTag(id); //! pendiente cambiar el id por data-id (falta pintarlo en pintarBotones()) para imprimir el nombre que corresponda a la etiqueta
+            pintarFotos(id);
+        }
+
+    })
+
+
+
+
+    //*** FUNCIONES ***//
+
+    const pintarBotones = () => { //! quizás utilizar un único array para pintar todo
+
+        arrayBotones.forEach((item) => {
+            const botones = document.createElement('BUTTON');
+            botones.textContent = item[0];
+            botones.id = item[1];
+            //botones.setAttribute("data-id", item[0]); //! pendiente hacer
+
+            capBotones.append(botones);
+
+        })
+
+    } //!FUNC-PINTARBOTONES
+
+
+
+    const filtrarTag = (id) => {
+
+        const fotos = arrayFotos.filter((item) => item.id == id);
+
+        return console.log(`Se han encontrado ${fotos.length} fotos con la etiqueta ${id}.`); //! pendiente modificar el mensaje para cuando se vaya a pintar 
+    }
+
+
+
+    const pintarTag = (id) => { //! incompleta: no funciona bien
+
+        capTextoTag.innerHTML = '';
+
+        filtrarTag(id); //! no funciona(?)
+
+        const elementP = document.createElement('P');
+        elementP.textContent = `Se han encontrado ${filtrarTag(id)} fotos con la etiqueta ${id}.`
+
+        capTextoTag.append(elementP);
+        
+    } //!FUNC-PINTARTAG
+
+
+
+    const pintarFotos = (id) => {
+
+        capFotos.innerHTML = '';
+
+        arrayFotos.forEach((item) => {
+            if(item.id == id){
+                const elementImg = document.createElement('IMG');
+                elementImg.src = urlBase + item.url;
+                elementImg.title = item.title;
+                elementImg.alt = item.alt;
+                const elementHeader = document.createElement('H5');
+                elementHeader.textContent = item.header;
+    
+                capFotos.append(elementImg, elementHeader);
+
+            }
+
+        })
+
+    } //!FUNC-PINTARFOTOS
+
+
+
+    const init = () => {
+
+        pintarBotones();
+
+    } //!FUNC-INIT
+
+
+    init();
+
+
+}) //!LOAD
